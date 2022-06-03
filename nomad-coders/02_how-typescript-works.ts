f0d30a973ce9037be9d43d9de7fd721a5a71cf8e
greencoffee1635 <166354@naver.com>
2022-06-03 15:38:37 +0900

    How TypeScript works

---
+++ nomad-coders/02_how-typescript-works.ts
@@ -0,0 +1,16 @@
+[1, 2, 3, 4] + false;
+// '+' 연산자를 'number[]' 및 'boolean' 형식에 적용할 수 없습니다.
+
+function divide(a, b) {
+  return a / b;
+}
+
+divide("hello");
+// 2개의 인수가 필요한데 1개를 가져왔습니다.
+
+const child = {
+  age: 12,
+};
+
+child.age = false;
+// 'boolean' 형식은 'number' 형식에 할당할 수 없습니다.
