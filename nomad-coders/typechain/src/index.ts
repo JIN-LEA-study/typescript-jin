a5748ba3efb6aae14201a157cc0cb9f469c76854
greencoffee1635 <166354@naver.com>
2022-06-22 19:45:34 +0900

    Declaration Files

--- nomad-coders/typechain/src/index.ts
+++ nomad-coders/typechain/src/index.ts
@@ -1,11 +1,9 @@
-const greeting = () => "hi";
+// myPackage가 node_module인 것처럼 생각
 
-class Block {
-  constructor(private data: string) {}
-  static hello() {
-    return "hi";
-  }
-}
+import { init, exit } from "myPackage";
 
-window.Array()
-localStorage.setItem()
+init({ url: "true" });
+
+exit(1);
+
+localStorage; // lib.dom.d.ts - 타입 스크립트가 localStorage가 뭔지 아는 방법, 위 코드는 그에 대한 예시이다.
