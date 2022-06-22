a234d7942503fc3ab6e6a6bcbf05d4af7633c5e7
greencoffee1635 <166354@naver.com>
2022-06-23 00:51:18 +0900

    JSDoc

--- nomad-coders/typechain/src/index.ts
+++ nomad-coders/typechain/src/index.ts
@@ -1,9 +1,4 @@
 // myPackage가 node_module인 것처럼 생각
+import { init, exit } from "./myPackage";
 
-import { init, exit } from "myPackage";
-
-init({ url: "true" });
-
-exit(1);
-
-localStorage; // lib.dom.d.ts - 타입 스크립트가 localStorage가 뭔지 아는 방법, 위 코드는 그에 대한 예시이다.
+init();
