7a9034acee024f7f7c7d1d3df70baf0dd04430ef
greencoffee1635 <166354@gmail.com>
2022-08-07 16:06:33 +0900

    chore: tailwind 설정

--- carrot-market/pages/index.tsx
+++ carrot-market/pages/index.tsx
@@ -1,10 +1,14 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return null
-}
+  return (
+    <div className="bg-red-500">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
